2cb2e1d1eae06c4114c5ff1dc6fe7295622dd503
Zpage <Zpage@PORT-STANLEY>
2022-11-04 23:30:54 -0400

    basic test working, TODO build out helper functions

---
+++ ConfigurationManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailClientLibrary
+{
+    public class ConfigurationManager
+    {
+        protected readonly IConfiguration _configuration;
+
+        public ConfigurationManager()
+        {
+           
+        }
+
+        
+    }
+}
--- EmailClient.cs
+++ EmailClient.cs
@@ -1,11 +1,113 @@
-﻿using System.Net;
-using System.Net.Mail;
-using System.Net.Mime;
+﻿using System;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+using MailKit.Security;
 
-namespace EmailClient
+namespace EmailClientLibrary
 {
     public class EmailClient
     {
+        private static string recipient;
+        private static MimeMessage message;
 
+        public static string TestSendEmail()
+        {
+            string log = "";
+            message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress("Margret", "margret76@ethereal.email"));
+
+            //Sending to test email for now, will swap with destination address from input
+            message.To.Add(MailboxAddress.Parse("margret76@ethereal.email"));
+
+            message.Subject = "Whoa! It worked!!!";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Yes,
+                   Hello West World..."
+            };
+
+            SmtpClient client = new SmtpClient();
+
+            try
+            {
+                client.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+                client.Authenticate("margret76@ethereal.email", "u5aTHzneNVSzfSXapp");
+                Console.WriteLine("Just tried login");
+                client.Send(message);
+                log += "\nJust tried send " + message.Body.ToString();
+            }
+            catch (Exception ex)
+            {
+                log += "\n" + ex.Message;
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+                log += "\nComplete";
+            }
+            return log;
+        }
+
+        public void CreateEmail(string subject, string body)
+        {
+            message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress("Margret", "margret76@ethereal.email"));
+
+            //Sending to test email for now, will swap with destination address from input
+            message.To.Add(MailboxAddress.Parse("margret76@ethereal.email"));
+
+            message.Subject = subject;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @body
+            };
+        }
+
+        public void SendEmail()
+        {
+            SmtpClient client = new SmtpClient();
+
+            try
+            {
+                client.Connect("smtp.ethereal.com", 587, SecureSocketOptions.StartTls);
+                client.Authenticate("margret76@ethereal.email", "u5aTHzneNVSzfSXapp");
+                Console.WriteLine("Just tried login");
+                client.Send(message);
+                Console.WriteLine("Just tried send " + message.Body.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+                Console.WriteLine("Complete");
+            }
+        }
+
+        public void SetRecipient(string recipient)
+        {
+            
+        }
+
+        private void LogInfo()
+        {
+            //Setup Logging class
+            //Get log path from config
+        }
+
+        private void ResetClient()
+        {
+
+        }
     }
 }
